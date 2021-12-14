5d6766e146d707f1a475c75882c8b8ab0a22b181
tamaravasovic <vasovictamara8@gmail.com>
2021-12-14 12:11:27 +0100

    Inicijalizacija samog pocetka, pravljenje baze

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include("includes/header.php") ?>
+<div class="container-fullwidth">
+   <section class="banner">
+      <div class="container fill">
+         <div class="banner-text fill">
+            <div class="row fill align-items-center">
+               <div class="col-md-9 col-lg-8">
+                  <h1 class="text-uppercase text-center">Ski Servis Pahuljica</h1>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+</div>
+<?php include("includes/footer.php") ?>

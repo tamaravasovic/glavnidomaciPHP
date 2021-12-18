ed97ce0d87842f99cce3258cc54e54426368381c
tamaravasovic <vasovictamara8@gmail.com>
2021-12-18 19:12:57 +0100

    db dodatno

--- functions/db.php
+++ functions/db.php
@@ -14,4 +14,24 @@
     return htmlentities($string);
 
   }
+  function query($query){
+    global $con;
+    return mysqli_query($con, $query);
+  }
+
+  function fetch_array($result){
+    global $con;
+    return mysqli_fetch_array($result);
+  }
+  function get_last_id(){
+    global $con;
+    $last_id = $con->insert_id;
+    return $last_id;
+  }
+  function confirm($result){
+    global $con;
+    if(!$result){
+      die("QUERY FAILED" . mysqli_error($con));
+    }
+  }
   ?>
